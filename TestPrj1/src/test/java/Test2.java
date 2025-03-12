012a6500ab412a21adb29ce28838b9ad9bf02808
SUYOG YEOLE <SUYOG YEOLE@DESKTOP-C59MQFQ>
2025-03-12 20:06:03 +0530

    5th Commit

--- TestPrj1/src/test/java/Test2.java
+++ TestPrj1/src/test/java/Test2.java
@@ -1,6 +1,7 @@
-public class Test2{
+public class Test2 {
 
-  public static void main (Sting[] args){
-    //Edited by User2
-
-  }
+	public static void main(String[] args) {
+		// Edited by User2
+		System.out.println("Test2 Updated");
+	}
+}
